96fcbf9fea51f9154f936d6946d6bd4b2427a457
the3flower <flowerthreeethe@gmail.com>
2025-02-04 01:57:08 +0700

    enumerate wat??

--- experiment_syntax/python_syntax.py
+++ experiment_syntax/python_syntax.py
@@ -1,2 +1,11 @@
 print("Hello, World!")
 
+# enumerate
+def enumerate_test(arr):
+    for i, num in enumerate(arr):
+        print(f"enumerated type of array: {type(arr)}")
+
+        pass
+
+arr = ["Feel", "Noob", "RN"]
+print(enumerate_test(arr))
