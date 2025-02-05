79f607c7bced429d5c29bec3ab9a033f06743269
the3flower <flowerthreeethe@gmail.com>
2025-02-06 02:10:17 +0700

    math problem mini_max

--- problems/math/mini_max.py
+++ problems/math/mini_max.py
@@ -1,3 +1,26 @@
-def mini_max(arr):
+def mini_max_v1(arr):
+    # Write your code here
+    new_arr = []
+    result = []
+    
+    for i in range(len(arr)):
+        # new_arr = arr This can leading unexpected behavior
+        new_arr = arr[:]
+        new_arr.pop(i)
+        
+        sum = 0
+        
+        for j in new_arr:
+            sum += j
+            
+        result.append(sum)
+        
+    return min(result), max(result)
+
+def mini_max_v2(arr):
+    pass
 
 
+    
+arr = [1, 2, 3, 4, 5]
+print(mini_max_v1(arr))
