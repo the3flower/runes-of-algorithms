6cdc44d2927057619988fb8740ee269a10ac0d2a
the3flower <flowerthreeethe@gmail.com>
2025-02-05 23:39:01 +0700

    add many things man i don't know what to write in this commit message

--- experiment_syntax/python_syntax.py
+++ experiment_syntax/python_syntax.py
@@ -1,6 +1,6 @@
-print("Hello, World!")
+print("START")
 
-# enumerate
+# How 'enumerate' work?
 def enumerate_test(arr):
     for i, num in enumerate(arr):
         print(f"enumerated type of array: {type(arr)}")
@@ -8,4 +8,8 @@ def enumerate_test(arr):
         pass
 
 arr = ["Feel", "Noob", "RN"]
-print(enumerate_test(arr))
+print(len(arr))
+# print(enumerate_test(arr))
+
+# How 'array' work?
+# * I faced 'index out of range' for multiple times and never remember it SKILL ISSUES
---
+++ problems/math/mini_max.py
@@ -0,0 +1,3 @@
+def mini_max(arr):
+
+
