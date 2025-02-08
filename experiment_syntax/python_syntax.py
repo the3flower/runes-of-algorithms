240066ff2bb9a15e1ec0762278653b1074447d7a
the3flower <flowerthreeethe@gmail.com>
2025-02-08 20:21:06 +0700

    learn enumerate and split

--- experiment_syntax/python_syntax.py
+++ experiment_syntax/python_syntax.py
@@ -1,15 +1,25 @@
-print("START")
+print("THIS IS FOR EXPERIMENT PYTHON SYNTAX")
+print("\n")
 
 # How 'enumerate' work?
+print("[Enumerate]")
 def enumerate_test(arr):
     for i, num in enumerate(arr):
-        print(f"enumerated type of array: {type(arr)}")
-
-        pass
+        print(i)
+        print(f"enumerated type of i: {type(i)}")
+        print(num)
+        print(f"enumerated type of n {type(num)}")
 
 arr = ["Feel", "Noob", "RN"]
-print(len(arr))
-# print(enumerate_test(arr))
+print(f"Length of array: {len(arr)}")
+enumerate_test(arr)
+print("\n")
 
 # How 'array' work?
 # * I faced 'index out of range' for multiple times and never remember it SKILL ISSUES
+
+# Split
+print("[Split]")
+time = "14:05:30AM"
+time = time.split("AM")
+print(time)
