c2b02a0b4a225b33b4f3637f93be8ac985cf1427
the3flower <flowerthreeethe@gmail.com>
2025-02-14 00:12:49 +0700

    am not consit

---
+++ problems/time_conversion.py
@@ -0,0 +1,42 @@
+def time_conversion_v1(s):
+    if s[-2:] == "AM":
+        print("[AM]")
+        s1 = s.replace("AM"," ")
+
+        if s1[0:3] == "12":
+            s1[0:3] = "00"
+            print(s1)
+
+        print(s1) 
+
+    elif s[-2:] == "PM":
+        print("[PM]")
+        s1 = s.replace(s[-2:]," ")
+
+        if s1[0:2] == "12":
+            pass
+        else:
+            change_to_int = int(s1[0:2])
+            change_to_int = change_to_int + 12
+            change_to_str = str(change_to_int)
+            s1 = s1.replace(s[0:2], change_to_str)
+
+        print(s1) 
+
+def time_conversion_v2(s):
+    if s[-2:] == "AM":
+        s1 = s[:-2]  # Remove AM/PM part
+        if s1[:2] == "12":
+            s1 = "00" + s1[2:]  # Convert 12 AM to 00
+        return s1  # Return result instead of printing
+    
+    elif s[-2:] == "PM":
+        s1 = s[:-2]  # Remove AM/PM part
+        if s1[:2] != "12":  # Convert PM hours except 12 PM
+            change_to_int = int(s1[:2]) + 12
+            s1 = str(change_to_int) + s1[2:]
+        return s1  # Return result instead of printing
+
+s = "12:01:00AM" # should be 00:01:00
+s = "01:01:00PM"
+time_conversion(s)
