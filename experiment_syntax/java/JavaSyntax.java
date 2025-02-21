c08bac134fca0afc1eb09529c1f261ea2f26ce61
the3flower <flowerthreeethe@gmail.com>
2025-02-21 22:55:18 +0700

    java

---
+++ experiment_syntax/java/JavaSyntax.java
@@ -0,0 +1,10 @@
+// Class and Method
+public class JavaSyntax {
+    public static void main(String[] args) {
+        System.out.println("[This is Class and Method in Java]");
+        System.out.println("You need to Compile Java file first by javac <filename>.java");
+        System.out.println("After that it will generate .class file (bytecode)");
+        System.out.println("JVM will read .class file by java <filename>");
+    }
+}
+
