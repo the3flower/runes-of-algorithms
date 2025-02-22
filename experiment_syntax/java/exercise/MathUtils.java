6875824eb957f9d7bcfc291789864b12d9893c9e
the3flower <flowerthreeethe@gmail.com>
2025-02-23 01:04:28 +0700

    again java same exercise

--- experiment_syntax/java/exercise/MathUtils.java
+++ experiment_syntax/java/exercise/MathUtils.java
@@ -5,11 +5,14 @@ class MathUtils {
 
     public static void printSquare(int x) {
         int result = square(x);
-        System.out.print("Square of " + x);
-        System.out.print(" is " + result);
+        // System.out.print("Square of " + x);
+        // System.out.print(" is " + result);
+        System.out.println("Square of " + x + " is " + result);
+
     }
 
     public static void main(String[] args) {
-        MathUtils.printSquare(5);
+        // MathUtils.printSquare(5);
+        MathUtils.square(5); // in this case CAN access private method because main is in the same class
     }
 }
