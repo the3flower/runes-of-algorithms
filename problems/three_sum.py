ad8be6d007367df37ca72354489677e4de650f95
the3flower <flowerthreeethe@gmail.com>
2025-03-16 20:34:08 +0700

    new logic

--- problems/three_sum.py
+++ problems/three_sum.py
@@ -1,18 +1,12 @@
-def three_sum(nums):
-    num_hash = {}
-
-    pass
-
-nums = [-1,0,1,2,-1,-4]
-# Output: [[-1,-1,2],[-1,0,1]]
 """
-return x + y + z = 0
+return [x, y, z] (x + y + z = 0)
 
-x + y = -z
+x + y = -z (?)
 
 solution 1
     - create hash by dict
     - loop for pick two values
+        - pick up two values
         - if complement of two sum values is in hash, return complement and two values
             x = 2
             y = -1
@@ -21,7 +15,16 @@ solution 1
             complement = x + y + 0
 
             -1 = 2 + (-1) + 0
-        - if not add to hash
+        - or add complement to hash
+    - return three values (not a position)
 """
+
+def three_sum(nums):
+    num_hash = {}
+
+    pass
+
+nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
 
 print(three_sum(nums))
